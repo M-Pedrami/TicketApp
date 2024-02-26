912cddd7c8b875dc5fb97ce0b65698b9347c2dd0
Maziar Pedrami <awildroot@gmail.com>
2024-02-26 16:01:06 +0100

    added vercel json

--- app/page.js
+++ app/page.js
@@ -1,41 +1,41 @@
-import TicketCard from "./(components)/TicketCard";
+  import TicketCard from "./(components)/TicketCard";
 
-const getTickets = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/api/gettickets", {
-      cache: "no-store",
-    });
-    return response.json();
-  } catch (error) {
-    console.log("Error from getTickets/Dashboard", error);
-    throw new Error("something went wrong getting the tickets");
-  }
-};
+  const getTickets = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/gettickets", {
+        cache: "no-store",
+      });
+      return response.json();
+    } catch (error) {
+      console.log("Error from getTickets/Dashboard", error);
+      throw new Error("something went wrong getting the tickets");
+    }
+  };
 
-const Dashboard = async () => {
-  const { tickets } = await getTickets();
-  const uniqueCategories = [
-    ...new Set(tickets?.map(({ category }) => category)),
-  ];
-  return (
-    <div className="p-5 ">
-      <div>
-        {tickets &&
-          uniqueCategories.map((uniqueCategory, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h2>{uniqueCategory}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-                {tickets
-                  .filter((ticket) => ticket.category === uniqueCategory)
-                  .map((filteredTicket, _index) => (
-                    <TicketCard key={_index} ticket={filteredTicket} />
-                  ))}
+  const Dashboard = async () => {
+    const { tickets } = await getTickets();
+    const uniqueCategories = [
+      ...new Set(tickets?.map(({ category }) => category)),
+    ];
+    return (
+      <div className="p-5 ">
+        <div>
+          {tickets &&
+            uniqueCategories.map((uniqueCategory, categoryIndex) => (
+              <div key={categoryIndex} className="mb-4">
+                <h2>{uniqueCategory}</h2>
+                <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                  {tickets
+                    .filter((ticket) => ticket.category === uniqueCategory)
+                    .map((filteredTicket, _index) => (
+                      <TicketCard key={_index} ticket={filteredTicket} />
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Dashboard;
+  export default Dashboard;
